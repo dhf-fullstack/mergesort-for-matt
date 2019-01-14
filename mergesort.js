f8d4770e08b2dfb03e1ca553fb7c5cfdd627e03f
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-01-14 15:53:17 -0500

    working mergesort

--- mergesort.js
+++ mergesort.js
@@ -11,3 +11,42 @@ function split(arr) {
     return [firstHalf, secondHalf];
   }
 }
+
+function merge(l, r) {
+  let li = 0;
+  let ri = 0;
+  let result = [];
+  while (li < l.length && ri < r.length) {
+    if (l[li] < r[ri]) {
+      result = result.concat(l[li++]);
+    } else {
+      result = result.concat(r[ri++]);
+    }
+  }
+  if (li < l.length) {
+    result = result.concat(l.slice(li));
+  } else if (ri < r.length) {
+    result = result.concat(r.slice(ri));
+  }
+  return result;
+}
+
+function mergesort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  console.log(`arr: ${arr}`)
+  let splits = split(arr);
+  console.log(`splits: ${splits}`)
+  if  (splits.length === 0) {
+    return [];
+  }
+  let [l, r] = splits;
+  console.log(`before recursion l: ${l}, r: ${r}`)
+  l = mergesort(l);
+  r = mergesort(r);
+  console.log(`after recursion l: ${l}, r: ${r}`)
+  let result = merge(l,r);
+  console.log(`after merge ${result}`)
+  return result
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -9,3 +9,32 @@ describe('Split Array function', function() {
     expect(split([10, 20])).toEqual([[10], [20]]);
   });
 });
+
+describe('Merge two sorted array', function() {
+  it('it is able to merge two empty arrays', function() {
+    expect(merge([],[])).toEqual([]);
+  })
+  it('it is able to merge one non-empty with one empty array', function() {
+    expect(merge([1],[])).toEqual([1]);
+  })
+  it('it is able to merge two non-empty arrays', function() {
+    expect(merge([1,2,3],[4,5,6])).toEqual([1,2,3,4,5,6]);
+  })
+})
+describe('Merge Sort an array', function() {
+  it('it is able to sort an empty array', function() {
+    expect(mergesort([])).toEqual([]);
+  })
+  it('it is able to merge a singleton array', function() {
+    expect(mergesort([1])).toEqual([1]);
+  })
+  it('it is able to merge a sorted array', function() {
+    expect(mergesort([1,2,3])).toEqual([1,2,3]);
+  })
+  it('it is able to merge a silly array', function() {
+    expect(mergesort([2,1,-19,3,4])).toEqual([-19,1,2,3,4]);
+  })
+  it('it is able to merge a crazy array', function() {
+    expect(mergesort([-19, 27, 1 , 2, 45, 282, 7, 79, 134])).toEqual([-19, 1, 2, 7, 27, 45, 79, 134, 282]);
+  })
+})
